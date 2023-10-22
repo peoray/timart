51140fcc6851a35d6d42eece703cb84960e91d7e
peoray <peoray@yahoo.com>
2023-10-22 13:55:24 +0100

    feat(sql): Add environment variables

--- sql/dbConnection.js
+++ sql/dbConnection.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2/promise')
+require('dotenv').config()
 
 const main = async () => {
   try {
     const connection = await mysql.createConnection({
       multipleStatements: true,
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'password',
-      database: 'timart',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     })
     return connection
   } catch (error) {
